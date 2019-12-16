2f5811c5a0069758a85fc4b9af230aacf2b92e0a
Lyira <49322964+Lyira@users.noreply.github.com>
2019-12-16 14:00:39 +0200

    Update index.html

--- js/main.js
+++
@@ -1,8 +0,0 @@
-let fa = document.getElementsByClassName('fa');
-let active = document.getElementsByClassName('mobilMenu');
-fa[1].onclick = function() {
-  active[0].classList.add("active");
-}
-fa[0].onclick = function() {
-  active[0].classList.remove("active");
-}
